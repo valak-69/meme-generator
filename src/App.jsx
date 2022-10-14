39b7a43f24ef086ba7f0fda4fb5b97706d8f0332
valak-69 <valaknoob@gmail.com>
2022-10-14 17:40:28 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import Navbar from "./components/navbar";
 import Meme from "./components/meme";
 
 export default function App(){
+    
     return(
         <div>
+            <Navbar />
             <Navbar />
             <Meme />
         </div>
