fe4788c155e3ba740394ae4193cbe1ddc844939f
valak-69 <valaknoob@gmail.com>
2022-10-17 09:31:53 +0530

    updated files

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ export default function App(){
     return(
         <div>
             <Navbar />
-            <Navbar />
+            {/* <Navbar /> */}
             <Meme />
         </div>
     )
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,7 +2,7 @@
 
 export default function Navbar(){
     return(
-        <nav>
+        <nav className="try">
             <img className='header-image' src="../../Troll-Face.png" alt="" />
             <h3 className='nav-logo-text'>Meme Generator</h3>
             <h4 className='nav-title'>React Course - Project 3</h4>
